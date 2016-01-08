b936edc582b0ef95f60c6e03e174acfc59d9e7b0
Kabbaj Amine <amine.kabb@gmail.com>
2016-01-08 13:20:13 +0300

    Improve git repo updating script: * A better code * A better output * A better logic * Simple error(s) catch system

--- update_scripts.sh
+++ update_scripts.sh
@@ -3,7 +3,7 @@
 #######################################################################
 # Author            : Kabbaj Amine
 # Date Creation     : 2015-08-30
-# Last modification : 2015-11-12
+# Last modification : 2016-01-08
 
 # DESCRIPTION
 # - Update all git projects in this directory.
@@ -37,14 +37,18 @@ yellow="\033[33m"
 #		FUNCTIONS
 # #####################
 
-showTitle () {
-	# showTitle "title"
-	# showTitle "title" ["sub" (Sub-title) ]
+Echo () {
+	# If $1 is:
+	# - 0, echo $2 in yellow (ok).
+	# - 1, echo in green (upd).
+	# - 2, echo in red (err).
 
-	if [[ $2 = "sub" ]]; then
-		echo -e $yellow"********** $1 ***********\n"$white
+	if [ ${1} == 0 ]; then
+		echo -e "${yellow}[OK] ${d} (${git_branch}) ${white}"
+	elif [ ${1} == 1 ]; then
+		echo -e "${green}[UPD] ${d} (${git_branch}) ${white}"
 	else
-		echo -e $green"\n~~~~~~~~~~ $1 ~~~~~~~~~~\n"$white
+		echo -e "${red_bold}[ERR] ${d} (${git_branch}) ${white}"
 	fi
 }
 
@@ -52,26 +56,55 @@ showTitle () {
 #		PROCESSING
 # #####################
 
+echo -e "\n${green}####################################"
+echo -e "\tGIT REPOS UPDATE\t"
+echo -e "####################################${white}\n"
+
 if [ ! -d $proj_dir ]
 then
-	echo -e "$red""ERROR: $white""The directory $red_bold""$proj_dir""$white doesn't exist" &&
+	echo -e "${red}ERROR: ${white}The directory ${red_bold}${proj_dir}${white} doesn't exist" &&
 	kill -SIGINT $$
 else
-	echo -e "In the directory $red_bold""$proj_dir""$white:"
+	echo -e "In the directory ${red_bold}${proj_dir}${white}:"
 fi
 
+updated=0
+errors=0
 for d in $(ls -F $proj_dir | grep '/$' | cut -d '/' -f 1)
 do
-	cd "$proj_dir""$d" &&
+	cd "${proj_dir}${d}" &&
 	if [ -d ".git" ]
 	then
-		git_branch=$(git branch | grep '^*' | cut -d " " -f 2)
-		showTitle "$d" && showTitle "$git_branch" "sub" &&
-		git pull origin "$git_branch"
-	fi &&
+		git_branch=$(git branch  2> /dev/null | grep '^*' | cut -d " " -f 2)
+		git fetch origin ${git_branch} --quiet 2> /dev/null &&
+		git_log=$(git log HEAD..origin/${git_branch} --oneline 2> /dev/null | head -n 1)
+		# If git_log is empty, then no need to update
+		if [ $? != 0 ]; then
+			errors=$((errors+1))
+			Echo 2 ${d} ${git_branch}
+			# : go allows to quit condition (break-like)
+			# :
+		elif [ -z ${git_log} ]; then
+			Echo 0 ${d} ${git_branch}
+		else
+			Echo 1 ${d} ${git_branch}
+			git merge origin/${git_branch} --quiet
+			# Stock updated repos number
+			updated=$((updated+1))
+		fi
+	fi
 	cd ..
-done &&
+done
+if [ ${errors} != 0 ]; then
+	echo -e "${red_bold}---> ${errors}${white} error(s) occured"
+fi
+if [ ${updated} != 0 ]; then
+	echo -e "${green}---> ${updated}${white} repo(s) updated"
+else
+	echo -e "---> Nothing to update"
+fi
 
+echo -e "" &&
 cd "$curr_dir"
 
 IFS=$DEFAULT_IFS
