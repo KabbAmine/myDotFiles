8b6431e8863b3dc642a9060ba92fd11282861742
Kabbaj Amine <amine.kabb@gmail.com>
2016-08-31 10:47:40 +0300

    Install scripts: Fix calibre

--- install/scripts/calibre.sh
+++ install/scripts/calibre.sh
@@ -6,16 +6,20 @@ IFS='
 
 url="https://api.github.com/repos/kovidgoyal/calibre/releases/latest"
 
-if [ ! -x '/usr/bin/calibre' ]; then
+install() {
 	sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+}
+
+if [ ! -x '/usr/bin/calibre' ]; then
+	install
 else
-	current_version="$(calibre --version | awk '{print $3}' | tr -d ')').0"
+	current_version=$(calibre --version | awk '{print $3}' | tr -d ')')
 	last_version=$(curl -s $url | grep 'tag_name' | cut -d '"' -f 4 | tr -d 'v')
 
 	if [ "$current_version" == "$last_version" ]; then
 		echo "Already up-to-date"
 	else
-		sudo -v && wget -nv -O- https://raw.githubusercontent.com/kovidgoyal/calibre/master/setup/linux-installer.py | sudo python -c "import sys; main=lambda:sys.stderr.write('Download failed\n'); exec(sys.stdin.read()); main()"
+		install
 	fi
 fi
 
